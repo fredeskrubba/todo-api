954785fc7eb29fbe151bfd1211e57e8f2fee55ae
Fredeskrubba <frederikskrubbeltrang@proton.me>
2025-12-16 15:39:52 +0100

    initial commit

---
+++ todo-api/Controllers/TodoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using todo_api.Models;
+
+namespace todo_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var todos = new List<TodoItem>
+        {
+            new TodoItem
+            {
+                Id = 1,
+                Title = "Learn ASP.NET Core",
+                Description = "Understand MVC and Web APIs",
+                IsComplete = false
+            },
+            new TodoItem
+            {
+                Id = 2,
+                Title = "Build Todo API",
+                Description = "Create controllers and endpoints",
+                Color = "#f9c74f",
+                IsComplete = false
+            },
+            new TodoItem
+            {
+                Id = 3,
+                Title = "Push to GitHub",
+                Description = "Create repository and commit code",
+                Color = "#f94144",
+                IsComplete = true
+            }
+        };
+
+            return Ok(todos);
+        }
+    }
+}
---
+++ todo-api/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace todo_api.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Color { get; set; } = "#61bd92";
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
